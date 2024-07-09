4bafad73469f8c6a34039bf1ba173675d4c2efc5
Daniel <dbellonzi@aol.com>
2024-07-09 14:06:16 -0700

    new coinflip project

---
+++ week1/practice/coinFlip/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main( string[] args )
+    {
+        // Randomly pick a heads or tails
+        // Keep track of heads and tails flipped
+
+        // Variables
+        int heads = 0;
+        int tails = 0;
+        int count;
+        int flip;
+        string countString;
+
+        // Random variable
+        Random rand = new Random();
+
+
+        Console.Write("How Many coin flips should I do? ");
+        countString = Console.ReadLine();
+
+        count = Int32.Parse(countString);
+
+        for( int i = 0; i < count; i++ )
+        {
+            flip = rand.Next(20);
+            if( flip % 2 == 0 ) 
+            {
+                Console.WriteLine($"Coin flip {i+1} was heads");
+                heads++;
+            }
+            else{
+                Console.WriteLine($"Coin flip {i+1} was tails");
+                tails++;
+            }
+        }
+
+        Console.WriteLine($"The final count was {heads} heads and {tails} tails");
+
+    }
+}
