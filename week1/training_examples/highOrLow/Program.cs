c005ddff9f61abd45c0e723d255d4392d7566b00
Daniel <dbellonzi@aol.com>
2024-07-09 13:17:59 -0700

    day 2: starting .Net and C#, highlow and myfirstconsole

---
+++ week1/training_examples/highOrLow/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    static void Main( string[] args )
+    {
+        /*
+            In Software Development Life Cycle (SDLC) a user story is a way to track a feature
+        */
+        // computer should pick a random number
+        // prompt user to guess numbeR
+        // if the guess is higher or lower than target number, couputer should say so
+        // user should keep guessing until they guess the correct number
+        // user wins when correct number is guessed
+
+
+        // Variables: 
+        int targetNumber;
+        int guessNumber;
+        int roundCount;
+        string guessString;
+
+        // Create a random number
+        Random rand = new Random();
+        targetNumber = rand.Next(1001);
+        roundCount = 0;
+
+
+        do {
+            // Prompt user to make a guess and store it, iterate round count
+            Console.Write("Welcome to High or Low, please pick a number between 0 and 1000 to begin: ");
+            guessString = Console.ReadLine();
+            roundCount++;
+
+            // Parse user input for guessNumber
+            guessNumber = Int32.Parse(guessString);
+
+            // Compare guessNumber to targetNumber
+            if (guessNumber == targetNumber)
+            {
+                Console.WriteLine("You did it, Good job");
+            }
+            else if(guessNumber > targetNumber)
+            {
+                Console.WriteLine("Too high, try again: ");
+            } 
+            else
+            {
+                Console.WriteLine("Too low, try again: ");
+            }
+        }
+        while (guessNumber != targetNumber);
+
+        Console.WriteLine($"You guessed it in {roundCount} rounds!Thanks for playing!");
+
+    }
+}
---
+++ week1/training_examples/myFirstConsole/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+Console.WriteLine("Please enter your name for a personalized greeting: ");
+
+string username = "user";
+username = Console.ReadLine();
+
+// Console.WriteLine("Welcome to Revature: " + username); // String concatination
+// Console.WriteLine("Welcome to Revature: {0}", username); // String interpolation
+Console.WriteLine($"Welcome to Revature: {username}"); // String formatting
+
+/*
+    Variable Types:
+        Decimal
+        Int32
+        
+        Numeric(double, float, long, short, decimal)
+        Integer
+
+        Boolean
+
+        char
+        string
+
+        byte and bit
+
+    Conditional Statements & Control Flow:
+        if, else if, else
+        switch, case
+        try/catch/finally - exception handling
+
+    Looping:
+        for
+        do while, while
+        for each
+
+*/
+
+bool? runChoice = null;
+
+if( runChoice == true ) 
+{
+    Console.WriteLine("runChoice is true");
+}
+else if (runChoice == false )
+{
+    Console.WriteLine("runChoice is false");
+}
+else
+{
+    Console.WriteLine("runChoice is null");
+}
+
+/*
+    Comparison Operators:
+        ==, >, <, >=, <=, !=
+*/
+
